adeb10334cd5aa394a4227610d5f71b0157a86e8
nifist99 <nikofigit@gmail.com>
2023-02-27 19:57:26 +0700

    raidus

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -5,7 +5,7 @@
 
         <li class="nav-item @if($link=='dashboard') active @endif">
           <a class="nav-link" href="{{url('dashboard')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi  mdi-view-dashboard"></i>
             <span class="menu-title">Dashboard</span>
           </a>
         </li>
@@ -51,18 +51,18 @@
 
         {{-- account setting --}}
 {{-- 
-        <li class="menu-item @if($link=='account' or $link=='password') open active @endif">
-          <a href="javascript:void(0);" class="menu-link menu-toggle">
+        <li class="nav-item @if($link=='account' or $link=='password') open active @endif">
+          <a href="javascript:void(0);" class="nav-link menu-toggle">
             <i class="menu-icon tf-icons bx bx-user"></i>
             <div data-i18n="Account Settings">Account Settings</div>
           </a>
           <ul class="menu-sub">
-            <li class="menu-item @if($link=='account') active @endif ">
+            <li class="nav-item @if($link=='account') active @endif ">
               <a href="{{url('account')}}" class="menu-link">
                 <div data-i18n="Account">Account</div>
               </a>
             </li>
-            <li class="menu-item @if($link=='password') active @endif">
+            <li class="nav-item @if($link=='password') active @endif">
               <a href="{{url('password')}}" class="menu-link">
                 <div data-i18n="Notifications">Change Password</div>
               </a>
@@ -79,28 +79,28 @@
 
         <li class="nav-item @if($link=='cms_role') active @endif">
           <a class="nav-link" href="{{url('admin/role')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-key"></i>
             <span class="menu-title">Roles Management</span>
           </a>
         </li>
 
         <li class="nav-item @if($link=='users') active @endif">
           <a class="nav-link" href="{{url('admin/users')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-account"></i>
             <span class="menu-title">Users Management</span>
           </a>
         </li>
 
         <li class="nav-item @if($link=='cms_settings') active @endif">
           <a class="nav-link" href="{{url('admin/settings')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-settings"></i>
             <span class="menu-title">Settings</span>
           </a>
         </li>
 
         <li class="nav-item @if($link=='cms_logs') active @endif">
           <a class="nav-link" href="{{url('admin/logs')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-flag-outline"></i>
             <span class="menu-title">Log Users Access</span>
           </a>
         </li>
@@ -117,14 +117,14 @@
 
         <li class="nav-item @if($link=='cms_menus') active @endif">
           <a class="nav-link" href="{{url('admin/menus')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-menu"></i>
             <span class="menu-title">Menu Management</span>
           </a>
         </li>
 
         <li class="nav-item @if($link=='cms_modules') active @endif">
           <a class="nav-link" href="{{url('admin/modules')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-monitor-multiple"></i>
             <span class="menu-title">Module Generator</span>
           </a>
         </li>
@@ -132,14 +132,14 @@
 
         <li class="nav-item @if($link=='cms_emails') active @endif">
           <a class="nav-link" href="{{url('admin/emails')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-email"></i>
             <span class="menu-title">Email Templates</span>
           </a>
         </li>
 
         <li class="nav-item @if($link=='cms_document') active @endif">
           <a class="nav-link" href="{{url('admin/document')}}">
-            <i class="mdi mdi-grid-large menu-icon"></i>
+            <i class="menu-icon mdi mdi-book-multiple"></i>
             <span class="menu-title">Documentation</span>
           </a>
         </li>
